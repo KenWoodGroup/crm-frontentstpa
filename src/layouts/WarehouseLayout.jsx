2820deef37750c74de9829183900536ea2ed1196
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-10-19 18:33:54 +0500

    edit marge

--- src/layouts/WarehouseLayout.jsx
+++ src/layouts/WarehouseLayout.jsx
@@ -39,45 +39,6 @@ export default function WarehouseLayout() {
     ];
 
 
-        {
-            id: 1,
-            label: "Home Page",
-            path: "/warehouse/dashboard",
-            icon: LayoutDashboard
-        },
-        {
-            id: 2,
-            label: "Products",
-            path: "/warehouse/product",
-            icon: Package
-        },
-        {
-            id: 3,
-            label: "Kirim",
-            path: "/warehouse/stockin",
-            icon: PackagePlus
-        },
-        {
-            id: 4,
-            label: "Chiqim",
-            path: "/warehouse/stockout",
-            icon: PackageMinus,
-        },
-        {
-            id: 5,
-            label: "Chiqindi",
-            path: "/warehouse/disposal",
-            icon: Recycle
-        },
-        {
-            id: 5,
-            label: "Dilers",
-            path: "/warehouse/dilers",
-            icon: User
-        },
-        { path: "/warehouse/settings", label: "Settings", icon: Settings },
-
-    ]
     return (
         <div className={`transition-all duration-300 ${sidebarOpen ? "ml-64" : "ml-20"}`}>
             <SuperAdminSidebar
