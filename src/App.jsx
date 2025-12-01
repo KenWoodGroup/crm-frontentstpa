653b48bac36013b24df7ee39ef4240d0aebc1a33
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-12-01 14:58:43 +0500

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,25 @@
 // src/app/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { supperAdminRoutes } from "./routes/adminRoutes";
 import { userRoutes } from "./routes/userRoutes";
-
 import Login from "./Components/Login/Login";
 import ErrorPage from "./Components/ErrorPage/ErrorPage";
 import SuperAdminLayout from "./layouts/SuperAdminLayout";
 import MainLayout from "./layouts/MainLayout";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ForgotPassword from "./Components/ResetPassword/ForgotPassword";
 import WarehouseLayout from "./layouts/WarehouseLayout";
 import { warehouseRoutes } from "./routes/warehouseRoutes";
-// import DilerLayout from "./layouts/DilerLayout";
-import { dilertoutes } from "./routes/dilerRoutes";
 import AppLayout from "./layouts/AppLayout";
-import CompanyLayout from "./layouts/CompanyLayout";
-import { companyRoutes } from "./routes/companyRoutes";
-import CompanyWarehouseLayout from "./layouts/CompanyWarehouseLayout";
-import { companyWarehouseRoutes } from "./routes/companyWarehouseRoutes";
 import ManagerLayout from "./layouts/ManagerLayout";
 import { managerRoutes } from "./routes/managerRoutes";
 import Factory from "./Components/ProtectedRoutes/Factory";
 import SuperAdmin from "./Components/ProtectedRoutes/SuperAdmin";
 import Manager from "./Components/ProtectedRoutes/Manager";
 import Warehouse from "./Components/ProtectedRoutes/Warehouse";
-import Dealer from "./Components/ProtectedRoutes/Dealer";
-import IndependentLayout from "./layouts/IndependentLayout";
-import { independentRoutes } from "./routes/independentRoutes";
-import Independent from "./Components/ProtectedRoutes/Independent";
-import Company from "./Components/ProtectedRoutes/Company";
-import CompanyWarehouse from "./Components/ProtectedRoutes/CompanyWarehouse";
-import BuildingLayout from "./layouts/BuildingLayout";
-import { buildingRoutes } from "./routes/buildingRoutes";
+
 
 // React Query client
 const queryClient = new QueryClient();
@@ -87,62 +71,6 @@ function App() {
               </Warehouse>
             }>
               {warehouseRoutes?.map((r) => {
-                return (
-                  <Route key={r.name} path={r.path} element={r.element} />
-                )
-              })}
-            </Route>
-            {/* <Route element={
-              <Dealer>
-                <DilerLayout />
-              </Dealer>
-            }>
-              {dilertoutes?.map((r) => {
-                return (
-                  <Route key={r.name} path={r.path} element={r.element} />
-                )
-              })}
-            </Route> */}
-            <Route element={
-              <Independent>
-                <IndependentLayout />
-              </Independent>
-            }>
-              {independentRoutes?.map((r) => {
-                return (
-                  <Route key={r.name} path={r.path} element={r.element} />
-                )
-              })}
-            </Route>
-            <Route element={
-              <Company>
-                <CompanyLayout />
-              </Company>
-              // </ProtectedUsersRoute>
-            }>
-              {companyRoutes?.map((r) => {
-                return (
-                  <Route key={r.name} path={r.path} element={r.element} />
-                )
-              })}
-            </Route>
-            <Route element={
-              <CompanyWarehouse>
-                <CompanyWarehouseLayout />
-              </CompanyWarehouse>
-            }>
-              {companyWarehouseRoutes?.map((r) => {
-                return (
-                  <Route key={r.name} path={r.path} element={r.element} />
-                )
-              })}
-            </Route>
-            <Route element={
-              // <ProtectedUsersRoute>
-              <BuildingLayout />
-              // </ProtectedUsersRoute>
-            }>
-              {buildingRoutes?.map((r) => {
                 return (
                   <Route key={r.name} path={r.path} element={r.element} />
                 )
